06ac1dbfa634ffe0184f3ff6c5c5d58bf27e6024
fenyadim <dimos9813@gmail.com>
2025-01-30 23:22:52 +0800

    Написал e2e тесты

--- src/services/slices/user/user.test.ts
+++ src/services/slices/user/user.test.ts
@@ -33,42 +33,4 @@ describe('Тестирование user', () => {
     const { user } = newState;
     expect(user).toEqual(mockUser);
   });
-  // describe('Тестирование экшена createOrder', () => {
-  //   test('Pending process', () => {
-  //     const action = { type: createOrderThunk.pending.type };
-  //     const state = ordersSlice.reducer(initialOrdersState, action);
-  //     expect(state.isLoading).toBe(true);
-  //     expect(state.error).toBe(undefined);
-  //     expect(state.orderRequest).toBe(true);
-  //   });
-  //   test('Fulfilled process', () => {
-  //     const mockData = [
-  //       '643d69a5c3f7b9001cfa093c',
-  //       '643d69a5c3f7b9001cfa0941',
-  //       '643d69a5c3f7b9001cfa0941',
-  //       '643d69a5c3f7b9001cfa0941',
-  //       '643d69a5c3f7b9001cfa0941',
-  //       '643d69a5c3f7b9001cfa0941',
-  //       '643d69a5c3f7b9001cfa093c'
-  //     ];
-  //     const action = {
-  //       type: createOrderThunk.fulfilled.type,
-  //       payload: { order: mockData }
-  //     };
-  //     const state = ordersSlice.reducer(initialOrdersState, action);
-  //     expect(state.isLoading).toBe(false);
-  //     expect(state.error).toBe(undefined);
-  //     expect(state.orderModal).toEqual(mockData);
-  //     expect(state.orderRequest).toBe(false);
-  //   });
-  //   test('Rejected process', () => {
-  //     const action = {
-  //       type: createOrderThunk.rejected.type,
-  //       error: { message: 'error' }
-  //     };
-  //     const state = ordersSlice.reducer(initialOrdersState, action);
-  //     expect(state.isLoading).toBe(false);
-  //     expect(state.error).toBe('error');
-  //   });
-  // });
 });
